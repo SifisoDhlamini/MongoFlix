ea26ae2a0007ad5dd6fdcc8e9c970b891a7f97c6
Scifi_the_dev <sifisoldhlamini@outlook.com>
2022-04-11 00:14:23 +0800

    Create/Update Commemnts complete

--- mflix/db.py
+++ mflix/db.py
@@ -352,7 +352,13 @@ def add_comment(movie_id, user, comment, date):
     """
     # TODO: Create/Update Comments
     # Construct the comment document to be inserted into MongoDB.
-    comment_doc = {"some_field": "some_value"}
+    comment_doc = {
+        "movie_id": ObjectId(movie_id),
+        "name": user.name,
+        "email": user.email,
+        "text": comment,
+        "date": date
+    }
     return db.comments.insert_one(comment_doc)
 
 
@@ -366,8 +372,8 @@ def update_comment(comment_id, user_email, text, date):
     # Use the user_email and comment_id to select the proper comment, then
     # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        {"some_field": "some_value"},
-        {"$set": {"some_other_field": "some_other_value"}}
+        {"_id": ObjectId(comment_id), "email": user_email},
+        {"$set": {"text": text, "date": date}}
     )
 
     return response
@@ -436,7 +442,7 @@ def add_user(name, email, hashedpw):
         # Insert a user with the "name", "email", and "password" fields.
         # TODO: Durable Writes
         # Use a more durable Write Concern for this operation.
-        db.users.with_options(write_concern=WriteConcern(w=2)).insert_one({
+        db.users.with_options(write_concern=WriteConcern(w="majority")).insert_one({
             "name": name,
             "email": email,
             "password": hashedpw
@@ -533,8 +539,8 @@ def update_prefs(email, prefs):
         # TODO: User preferences
         # Use the data in "prefs" to update the user's preferences.
         response = db.users.update_one(
-            {"some_field": "some_value"},
-            {"$set": {"some_other_field": "some_other_value"}}
+            {"email": email},
+            {"$set": {"preferences": prefs}}
         )
         if response.matched_count == 0:
             return {'error': 'no user found'}
